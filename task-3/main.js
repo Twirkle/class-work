cdeb80d46640bc54cabf3fcbd4166b111e0959c8
Twirkle <saysf@bk.ru>
2024-01-23 11:29:25 +0300

    Выполнил задание по созданию многомерных массивов by twirkle

--- task-3/main.js
+++ task-3/main.js
@@ -2,5 +2,9 @@
 // console.log (students);
 // students [1] = 'Никита';
 // console.log (students);
-const colledge = [['Tom','Sam','Vladimir'],['Dasha','Oleg','Ivan']];
-console.log (colledge);
+// const colledge = [['Tom','Sam','Vladimir'],['Dasha','Oleg','Ivan']];
+// console.log (colledge);
+
+const marks = [['Марка автомобиля'],['Марка велосипеда'],['Марка мотоцикла'],['Марка самолета']];
+
+console.log (marks);
