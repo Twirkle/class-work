a834cc679b41ccf165ec022d76f850024526d749
Twirkle <saysf@bk.ru>
2024-02-01 12:45:34 +0300

    Выполнил 3 задание

---
+++ task-5/zd3.js
@@ -0,0 +1,11 @@
+function maxNumber(a, b) {
+    if (a >= b) {
+      return a;
+    } else {
+      return b;
+    }
+  }
+  
+  console.log(maxNumber(5, 10));
+  console.log(maxNumber(-2, 7)); 
+  console.log(maxNumber(0, 0)); 
