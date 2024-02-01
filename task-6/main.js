c782e5bcb068009837f68b92d3b52e99c4cded6d
Twirkle <saysf@bk.ru>
2024-02-01 13:40:08 +0300

    Выполнили стрелочную функцию

---
+++ task-6/main.js
@@ -0,0 +1,8 @@
+let minMax = (a,b) =>{
+if(a>b) console.log('Значение a =${a}больше значения b=${b}');
+else if (b>a) ('Значение b =${b}больше значения a=${a}')
+else if (a=== undefined || b=== undefined) console.log('Введитезначения');
+else console.log ('Значения введене некорректно')
+}
+
+minMax("dqdgrgeg",654);
